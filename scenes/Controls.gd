c7e3bd10e62841492bff02cca7451ff4e81054b5
Angelo Aracri <angeloaracri@yahoo.de>
2020-08-20 15:11:55 +0200

    Created simple menu structure

---
+++ scenes/Controls.gd
@@ -0,0 +1,4 @@
+extends CanvasLayer
+
+func _on_ButtonBack_pressed():
+	get_tree().change_scene("res://scenes/Menu.tscn")
---
+++ scenes/Menu.gd
@@ -0,0 +1,17 @@
+tool
+extends CanvasLayer
+
+func _ready():
+	$MarginContainer/VBoxContainer/VBoxContainer/ButtonControls.grab_focus()
+
+func _on_ButtonSingleplayer_pressed():
+	get_tree().change_scene("res://scenes/Game.tscn")
+
+func _on_ButtonCampaign_pressed():
+	get_tree().change_scene("res://scenes/Game.tscn")
+
+func _on_ButtonMultiplayer_pressed():
+	get_tree().change_scene("res://scenes/Game.tscn")
+
+func _on_ButtonControls_pressed():
+	get_tree().change_scene("res://scenes/Controls.tscn")
