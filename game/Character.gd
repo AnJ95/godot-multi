01ad5d2a68cbe1a5151df4996b220145d5bdafac
Angelo Aracri <angeloaracri@yahoo.de>
2020-08-23 04:39:35 +0200

    Added some bouncyness

--- game/Character.gd
+++ game/Character.gd
@@ -9,10 +9,13 @@ export(Color) var color = Color(0.8, 0.2, 0.2, 0.9) setget _on_set_color
 
 onready var player:Player = Multi.player(player_id)
 
-onready var sprite:Sprite = $Sprite
+onready var sprite = $Sprites
+onready var animation_player:AnimationPlayer = $AnimationPlayer
+
+onready var game_bounds = get_viewport_rect().grow(out_of_screen_tolerance)
 
 # some character vars
-const out_of_screen_tolerance = 50
+const out_of_screen_tolerance = 100
 const dash_power = 500
 const bounce_factor = 0.5
 const gravity = 300
@@ -23,8 +26,9 @@ var velocity = Vector2.ZERO
 
 var has_dashed = true
 
+var squish = 0
+
 func _ready():
-	$Label.text = str(health)
 	player.connect("controller_connection_changed", self, "_on_controller_connection_changed")
 	_on_controller_connection_changed()
 	_on_set_color(color)
@@ -49,15 +53,21 @@ func _physics_process(delta):
 	# Bounce off other
 	for slide_idx in range(get_slide_count()):
 		bounce_off(get_slide_collision(slide_idx), prev_vel)
-		prev_vel = Vector2(velocity.x, velocity.y)
-		
+		break
+	
+	# Shape
+	sprite.rotation = lerp_angle(sprite.rotation, Vector2.UP.angle_to(velocity), min(delta * 5, 1))
+	sprite.scale.y = 1 - squish
+	sprite.scale.x = 1 + squish
+	
+	squish -= squish * delta * 5
+	
 	# Die
-	if !get_viewport_rect().grow(out_of_screen_tolerance).has_point(global_position):
+	if !game_bounds.has_point(global_position):
 		velocity.x = 0
 		velocity.y = 0
 		health -= 1
 		has_dashed = true
-		$Label.text = str(health)
 		if health > 0:
 			emit_signal("respawn_character", self)
 		else:
@@ -71,19 +81,25 @@ func bounce_off(slide:KinematicCollision2D, prev_vel:Vector2):
 	var collider = slide.collider
 	if collider is KinematicBody2D:
 		collider.velocity -= slide.normal * bounce_factor * prev_vel.length()
+	
+	sprite.rotation = Vector2.UP.angle_to(slide.normal)
+	squish = min(squish + prev_vel.length() * 0.001, 1)
 
 func dash(direction:Vector2):
 	velocity = direction.normalized() * dash_power
 	has_dashed = true
+	
+	sprite.rotation = Vector2.UP.angle_to(velocity)
+	squish = max(squish -0.4, -1)
 		
 func _on_controller_connection_changed():
 	# if controller disconnected: show animation
 	if player.is_controller_connected():
 		$AnimationPlayer.stop()
-		$Sprite.modulate.a = 1
+		$Sprites.modulate.a = 1
 	else:
 		$AnimationPlayer.play("controller_disconnected")
 
 func _on_set_color(v):
 	color = v
-	$Sprite.modulate = color
+	$Sprites.modulate = color
