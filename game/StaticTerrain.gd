b8d6a52c566545f56121d0d6acd0ff6d5b53ba82
Angelo Aracri <angeloaracri@yahoo.de>
2020-08-23 20:03:07 +0200

    Added terrain texture and line

--- game/StaticTerrain.gd
+++ game/StaticTerrain.gd
@@ -1,5 +1,14 @@
+tool
 extends StaticBody2D
 
-
 func _ready():
-	$Polygon2D.polygon = $CollisionPolygon2D.polygon
+	$CollisionPolygon2D.polygon = $Polygon2D.polygon
+	
+	var size = $Polygon2D.polygon.size()
+	var path = PoolVector2Array()
+	path.resize(size + 1)
+	for i in range(size):
+		path[i] = $Polygon2D.polygon[i]
+	path[size] = $Polygon2D.polygon[0]
+		
+	$Line2D.points = path
